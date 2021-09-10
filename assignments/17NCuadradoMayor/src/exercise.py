0570d77d4fade1ac027edc192f84a900bce4c210
inesbojorquez <a01252378@itesm.mx>
2021-09-10 17:31:14 +0000

    Examenp2compu

--- assignments/17NCuadradoMayor/src/exercise.py
+++ assignments/17NCuadradoMayor/src/exercise.py
@@ -1,9 +1,14 @@
 
 
 def main():
-    numero = int(input("Escribe un numero : "))
     #escribe tu código abajo de esta línea
-
+    #Iniciar con una variable igual a 1, checar si la variable al cuadrado es mayor que N, 
+    # si no lo es, entonces incrementar la variable y repetir.
+    n = int(input("Escribe un numero : "))
+    r = 1
+    while n >=r**2:
+        r = r+1
+    print(r)                
 
 if __name__=='__main__':
     main()
